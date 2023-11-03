4aa7e6ecb130dce91f367ee1335e762870c1bd56
estebanmerlos33 <esteban-merlos33@outlook.com>
2023-11-03 17:09:56 -0300

    Added APIs and GitHub Actions

--- components/WeatherSearch.jsx
+++ components/WeatherSearch.jsx
@@ -1,43 +1,69 @@
-import { useState } from "react";
+const WeatherSearch = (props) => {
+  const searchWeather = async (city) => {
+    let coordinates = await getCoordinates(props.cityInput).then((c) => {
+      return c;
+    });
+    let latitude = await coordinates[1][1];
+    let longitude = await coordinates[2][1];
+    console.log(latitude);
+    console.log(longitude);
+    let url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weathercode`;
 
-const WeatherSearch = () => {
-  const [shownWeather, setShownWeather] = useState("");
-
-  const getWeather = async () => {
-    const city = "london";
-    const apiKey = "Rn83GNo+8YEZHpF0LDiXDA==ZnabpQj1Aw5ZZyny";
-
-    fetch(`https://api.api-ninjas.com/v1/geocoding?city=${city}`, {
-      method: "GET",
-      headers: {
-        "X-Api-Key": apiKey,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((result) => {
-        setShownWeather(result[0].name);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
+    let weatherCode = await fetch(url)
+      .then((data) => data.json())
+      .then((data) => {
+        return data.current.weathercode;
       });
+    props.handleSearch(weatherCode);
+  };
+
+  const getCoordinates = async (city) => {
+    const apiKey = process.env.COORDS_API_KEY;
+
+    try {
+      const response = await fetch(
+        `https://api.api-ninjas.com/v1/geocoding?city=${city}`,
+        {
+          method: "GET",
+          headers: {
+            "X-Api-Key": apiKey,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+
+      return Object.entries(result[0]);
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
+  };
+
+  const handleInputChange = (e) => {
+    props.handleInputChange(e);
   };
 
   return (
     <div>
       WeatherSearch<br></br>
       <br></br>
-      {shownWeather}
       <br></br>
-      <input type="text" placeholder="Buenos Aires"></input>
+      <input
+        value={props.cityInput}
+        onChange={(e) => handleInputChange(e)}
+        type="text"
+        placeholder="Buenos Aires"
+      ></input>
       <br></br>
       <br></br>
-      <button onClick={getWeather}>Buscar pronóstico</button>
+      <button onClick={searchWeather}>Buscar pronóstico</button>
     </div>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 
 import WeatherSearch from '../components/WeatherSearch.jsx'
+import WeatherBoard from '../components/WeatherBoard.jsx'
 import './App.css'
+import { useState } from 'react';
+
 
 function App() {
+
+  const [shownWeather, setShownWeather] = useState("");
+  const [cityInput, setCityInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setCityInput(e.target.value);
+  };
+  
+  const handleSearch = (updatedWeather) => {
+    console.log(updatedWeather)
+    setShownWeather(updatedWeather);
+  }
   
   return (
     <>
-      <WeatherSearch></WeatherSearch>
-      
+      <WeatherSearch cityInput = {cityInput} handleSearch = {handleSearch} handleInputChange = {handleInputChange}></WeatherSearch>
+      <WeatherBoard shownWeather = {shownWeather}></WeatherBoard>
     </>
   )
 }
