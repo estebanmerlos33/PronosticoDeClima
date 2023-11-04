31e706547cf7e2c5c4e0da0ab297fb5726cac548
estebanmerlos33 <esteban-merlos33@outlook.com>
2023-11-04 18:21:02 -0300

    Added APIs and GitHub Actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-
-import WeatherSearch from '../components/WeatherSearch.jsx'
-import WeatherBoard from '../components/WeatherBoard.jsx'
-import './App.css'
-import { useState } from 'react';
-
+import WeatherSearch from "../components/WeatherSearch.jsx";
+import WeatherBoard from "../components/WeatherBoard.jsx";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
   const [shownWeather, setShownWeather] = useState("");
   const [cityInput, setCityInput] = useState("");
 
   const handleInputChange = (e) => {
     setCityInput(e.target.value);
   };
-  
+
   const handleSearch = (updatedWeather) => {
-    console.log(updatedWeather)
+    console.log(updatedWeather);
     setShownWeather(updatedWeather);
-  }
-  
+  };
+
   return (
     <>
-      <WeatherSearch cityInput = {cityInput} handleSearch = {handleSearch} handleInputChange = {handleInputChange}></WeatherSearch>
-      <WeatherBoard shownWeather = {shownWeather}></WeatherBoard>
+      <WeatherSearch
+        cityInput={cityInput}
+        handleSearch={handleSearch}
+        handleInputChange={handleInputChange}
+      ></WeatherSearch>
+      <WeatherBoard shownWeather={shownWeather}></WeatherBoard>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
